b13497104f30f0ef4e9241119574e11f583fcca4
Ionut Dobrin <48881729+ionuthub@users.noreply.github.com>
2026-01-03 22:02:46 +0000

    Add AuthCallback route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const Index = lazy(() => import("./pages/Index"));
 const Terms = lazy(() => import("./pages/Terms"));
 const Privacy = lazy(() => import("./pages/Privacy"));
 const FAQ = lazy(() => import("./pages/FAQ"));
-const Auth = lazy(() => import("./pages/Auth"));
+const Auth = lazy(() => import("./pages/Auth"));const AuthCallback = lazy(() => import("./pages/AuthCallback"));
 const NotFound = lazy(() => import("./pages/NotFound"));
 
 const PageLoader = () => (
@@ -24,6 +24,7 @@ const PageLoader = () => (
       <Skeleton className="h-[400px]" />
       <Skeleton className="h-[400px] md:col-span-2" />
     </div>
+                        <Route path="/auth/callback" element={<AuthCallback />} />
   </div>
 );
 
